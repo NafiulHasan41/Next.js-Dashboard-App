43c7ac45b46b3efde2d6f64e0719f424aa9c7c60
NafiulHasan41 <nhashik40@gmail.com>
2024-10-05 21:32:14 +0600

    Added navbar section

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
+    <>
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
@@ -25,5 +26,10 @@ export default function InvoiceStatus({ status }: { status: string }) {
         </>
       ) : null}
     </span>
+
+   
+
+    </>
+    
   );
 }
